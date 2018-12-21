ff97fcba33f838c6530223e9bb184d13f38c5b36
mishalee <misha.lee@uq.net.au>
2018-12-21 16:52:34 +1000

    drew a platform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 500, 400
+speed = [1, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -14,13 +14,16 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+#    ballrect = ballrect.move(speed)
+#    if ballrect.left < 0 or ballrect.right > width:
+#        speed[0] = -speed[0]
+#    if ballrect.top < 0 or ballrect.bottom > height:
+#        speed[1] = -speed[1]
         
     screen.fill(black)
-    screen.blit(ball, ballrect)
+#    screen.blit(ball, ballrect)
+    white = (255, 255, 255)
+    pygame.draw.rect(screen, white, (225, 190, 50, 20))
+    pygame.draw.circle(screen, white, (225, 200), 10, 10)
+    pygame.draw.circle(screen, white, (275, 200), 10, 10)
     pygame.display.flip()
-    
