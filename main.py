05197b4e3dd5879e0ad7d5e117205d1c306ec982
MishaLee <cbaba58462@gmail.com>
2018-12-28 22:54:59 +1000

    background

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 500, 600  # SlinkyB window size
-# speed = [1, 1]
-black = 0, 0, 0
-
+# set up SlinkyB window
+size = width, height = 500, 600
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Slinky Bounce")  # Set title to the window
+backgroundIm = pygame.image.load("background.jpg")
+dead = False
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+# Colours
+black = (0, 0, 0)
+
+def text_objects(text, font, colour):
+    textSurface = font.render(text, True, colour)
+    return textSurface, textSurface.get_rect()
+
+
+# text_display
+def text_display(text, size, colour, x, y):
+    textFont = pygame.font.Font('freesansbold.ttf', size)
+    TextSurf, TextRect = text_objects(text, textFont, colour)
+    TextRect = (x, y)
+    screen.blit(TextSurf, TextRect)
+
+    # pygame.display.update()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+
+    screen.blit(backgroundIm, [0, 0])
         
 #    ballrect = ballrect.move(speed)
 #    if ballrect.left < 0 or ballrect.right > width:
 #        speed[0] = -speed[0]
 #    if ballrect.top < 0 or ballrect.bottom > height:
 #        speed[1] = -speed[1]
-        
-    screen.fill(black)
 #    screen.blit(ball, ballrect)
+
+    text_display('Score', 18, black, 10, 10)
+
     white = (255, 255, 255)
     pygame.draw.rect(screen, white, (225, 190, 50, 20))
     pygame.draw.circle(screen, white, (225, 200), 10, 10)
